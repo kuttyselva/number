433fb8b256ec07b866b31153548991a7242ad0d6
kutty selva <kuttyselva57@gmail.com>
2019-08-04 18:28:36 +0530

    number api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NumComponent } from './num/num.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule
